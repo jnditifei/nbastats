efb14729e740030096892330746915a33ac56829
jnditifei <jnditifei>
2021-03-18 10:07:24 +0100

    Bootstrap theme

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from dash import Dash
 import dash_bootstrap_components as dbc
 
-external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css', dbc.themes.CYBORG]
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css', dbc.themes.BOOTSTRAP]
 app = Dash(__name__, suppress_callback_exceptions=True, external_stylesheets=external_stylesheets)
 server = app.server
